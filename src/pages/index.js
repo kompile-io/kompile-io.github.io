0c84a38e75925877d0870308b158661139065f89
Alexis Kofman <alexis.kofman@gmail.com>
2021-05-09 18:39:44 +0200

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
+const headingAccentStyles = {
+  color: "#663399",
+}
+
+
+// markup
+const HomePage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Home Page</title>
+      <h1>
+        <span style={headingAccentStyles}>_ it kompiles ! </span>
+        <span role="img" aria-label="Party popper emojis">
+          🎉🎉🎉
+        </span>
+      </h1>
+    </main>
+  )
+}
+
+export default HomePage
